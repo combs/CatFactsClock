b7adb03d3697036c2c4449f2e8b1deeb9d404718
combs <ccombs@gmail.com>
2014-10-06 20:23:22 -0400

    initial

---
+++ cat-facts-clock.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+  $json_string = file_get_contents("http://catfacts-api.appspot.com/api/facts?number=1");
+
+  $parsed_json = json_decode($json_string);
+  $fact = $parsed_json->{'facts'}[0];
+	date_default_timezone_set('America/New_York');
+
+	$sentence= "It is " . ltrim(date('H'),"0") . " o'clock. $fact\n";
+	system('say -v Victoria "' . addslashes($sentence) . "\"");
+ 
+
+
+?>
